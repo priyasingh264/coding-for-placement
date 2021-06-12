9ee200591f3e85c7687caa4f55631f8787472e9a
Priya singh <72543764+priyasingh264@users.noreply.github.com>
2021-06-12 16:56:23 +0530

    Create trailingZeroOfFactorial.cpp

---
+++ trailingZeroOfFactorial.cpp
@@ -0,0 +1,32 @@
+
+
+#include <iostream>
+using namespace std;
+
+int trailingZero(int n)
+{
+    int sum = 0;
+    if (n<5)
+    {
+        return 0;
+    }
+    else
+    {
+        while(n!=0)
+        {
+            n = n/5;
+            sum = sum + n;
+        }
+    }
+    return sum;
+}
+
+
+int main()
+{
+    int fact;
+    cout<<"Enter the factorial whose trailing zeroes need to be counted : ";
+    cin>>fact;
+    cout<<"Number of trailing zeroes in factorial is : "<<trailingZero(fact);
+    return 0;
+}
