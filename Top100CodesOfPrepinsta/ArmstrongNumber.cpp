743a3e515db3b3d945d8e0fc5b2eaeaa037b6100
Priya singh <72543764+priyasingh264@users.noreply.github.com>
2021-06-13 22:13:03 +0530

    Update ArmstrongNumber.cpp

--- Top100CodesOfPrepinsta/ArmstrongNumber.cpp
+++ Top100CodesOfPrepinsta/ArmstrongNumber.cpp
@@ -7,15 +7,26 @@
 #include <iostream>
 #include<math.h>
 using namespace std;
+int countDigit(int n)
+{
+    int count =0;
+    while(n!=0)
+    {
+        n =n/10;
+        count++;
+    }
+    
+    return count;
+}
 
 int armstrong(int n)
 {
-    int sum=0,digitCount;
+    int sum=0,rem;
     int temp=n;
     while(n!=0)
     {
-        digitCount=n%10;
-        sum=sum+pow(digitCount,3);
+        rem=n%10;
+        sum=sum+pow(rem,countDigit(temp));
         n=n/10;
     }
     if(temp==sum)
