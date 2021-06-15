5b7ad246a75fb052d697ef690d01daf0545e9821
Priya singh <72543764+priyasingh264@users.noreply.github.com>
2021-06-15 17:22:46 +0530

    Create maximumNumberOfHandshake.cpp

---
+++ Top100CodesOfPrepinsta/maximumNumberOfHandshake.cpp
@@ -0,0 +1,20 @@
+
+//convert binary to decimal
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int max,p;
+cout<<"emter number of person"<<"\n";
+cin>>p;
+
+
+cout<<"maximum number of handshake"<<"\n";
+max=p*(p-1)/2;
+cout<<max;
+return 0;
+}
+
+
+
