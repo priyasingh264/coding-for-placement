f8399ffb016f2d00ae0db2b6036a1a407d4687d4
Priya singh <72543764+priyasingh264@users.noreply.github.com>
2021-06-17 11:41:16 +0530

    Create areaOfCircle.cpp

---
+++ Top100CodesOfPrepinsta/areaOfCircle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+float areaOFCircle(int r)
+{
+    float x;
+ float pi=3.14;
+x=pi*r*r;
+return x;
+}
+int main()
+{
+  cout<<areaOFCircle(2);
+  return 0;
+}
